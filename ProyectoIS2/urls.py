80d996aac76fba38c4c17adb2fe2506753599898
jotapoggi <jotapoggi@Jota-Satellite-P755>
2015-08-15 17:56:54 -0400

    modif de view

--- ProyectoIS2/urls.py
+++ ProyectoIS2/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url('^', include('django.contrib.auth.urls')),
+#    url(r'^principal/', views.principal, name='principal'),
 ]
