e154f474d37d086b1597154eed9ae56b48fad70f
alex <alex@alex>
2015-08-21 22:38:37 -0400

    Test pyunit

--- sis/tests.py
+++ sis/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from sis.models import MyUser
+
+class MyUserManagerTests(TestCase):
+
+    def test_create_user_is_an_instance_of_User(self):
+        """
+        create_user() deberia retornar un objeto user con nombre de usuario,
+        el email dado y contrasenha
+        """
+        usuario = MyUser.objects.create_user('testusuario', 'testusuario2@hotmail.com', 'pass123')
+        self.assertEqual(isinstance(usuario, MyUser),True)
+
+    def test_create_user_is_active_is_not_admin(self):
+        """
+        create_user() verificar si esta activo y no es admin
+        """
+        usuario = MyUser.objects.create_user('testusuario', 'testusuario2@hotmail.com', 'pass123')
+        self.assertEqual(usuario.is_admin,False)
+        self.assertEqual(usuario.is_active,True)
+        
+    def test_create_superuser_is_an_instance_of_User(self):
+        """
+        create_superuser() deberia retornar un objeto user con nombre de usuario,
+        el email dado y contrasenha
+        """
+        usuario = MyUser.objects.create_superuser('testusuario','testusuario2@hotmail.com', 'pass123')
+        self.assertEqual(isinstance(usuario, MyUser),True)
+        
+    def test_create_superuser_is_active_is_admin(self):
+        """
+        create_superuser() verificar si esta activo y no es admin
+        """
+        usuario = MyUser.objects.create_superuser('testusuario', 'testusuario2@hotmail.com', 'pass123')
+        self.assertEqual(usuario.is_admin,True)
+        self.assertEqual(usuario.is_active,True)
